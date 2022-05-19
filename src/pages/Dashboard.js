bb174ea6fe43d211d54132845500127575c0dab5
Malik Idrees <45917056+Malik-Idrees@users.noreply.github.com>
2022-05-20 03:45:50 +0500

    dashboard added

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,37 @@
 import React from 'react'
 
+// components
+
+import CardLineChart from '../components/Cards/CardLineChart'
+import CardBarChart from '../components/Cards/CardBarChart'
+import CardPageVisits from '../components/Cards/CardPageVisits'
+import CardSocialTraffic from '../components/Cards/CardSocialTraffic'
+
 const Dashboard = () => {
-    return <div className='dark:text-gray-50'>Dashboard</div>
+    return (
+        <>
+            <span className='text-xl p-4 mb-5 font-medium whitespace-nowrap dark:text-white'>
+                Dashboard
+            </span>
+
+            <div className='flex flex-wrap mt-3 sm:mt-5'>
+                <div className='w-full xl:w-8/12 mb-12 xl:mb-0 px-4'>
+                    <CardLineChart />
+                </div>
+                <div className='w-full xl:w-4/12 px-4'>
+                    <CardBarChart />
+                </div>
+            </div>
+            <div className='flex flex-wrap mt-4'>
+                <div className='w-full xl:w-8/12 mb-12 xl:mb-0 px-4'>
+                    <CardPageVisits />
+                </div>
+                <div className='w-full xl:w-4/12 px-4'>
+                    <CardSocialTraffic />
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Dashboard
