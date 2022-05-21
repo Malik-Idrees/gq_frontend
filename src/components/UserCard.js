3c6852cec576e5d52c823edf02063cb83a6b4d39
Malik Idrees <45917056+Malik-Idrees@users.noreply.github.com>
2022-05-21 22:04:05 +0500

    patched profile

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import userAvatar from '../assets/user_avatar.png'
 
-const UserCard = () => {
+const UserCard = ({ name, username, profession, expertiseLevel }) => {
     return (
         <div className='box-border rounded-2xl max-w-xl mx-auto shadow bg-gray-50 dark:bg-slate-800 sm:mt-10 mb-4 sm:mb-6 p-3'>
             <div className='flex justify-center items-center mx-auto relative sm:w-3/12 box-border w-6/12 px-6 sm:px-4'>
@@ -13,21 +13,23 @@ const UserCard = () => {
                 {/* Icon by Icon8 'https://icons8.com/icon/108639/user'> */}
             </div>
             <div className='text-lg text-center'>
-                <div className='text-blue-900 font-bold inline dark:text-blue-100'>
-                    Malik Idrees
-                </div>
+                <div className='text-blue-900 font-bold inline dark:text-blue-100'>{name}</div>
             </div>
-            <div className='text-blue-600 text-sm text-center my-2 dark:text-blue-100'>@midrs</div>
+            <div className='text-blue-600 text-sm text-center my-2 dark:text-blue-100'>
+                @{username}
+            </div>
             <div className='flex justify-between max-w-sm mx-auto border-t border-gray-200 dark:border-gray-600 mt-3 p-3 text-center'>
                 <div>
                     <p className='text-very-dark-desaturated-blue font-bold text-base'>
                         Profession
                     </p>
-                    <p className='text-xs'>Software Engineer</p>
+                    <p className='text-xs'>{profession}</p>
                 </div>
                 <div>
-                    <p className='text-very-dark-desaturated-blue font-bold text-lg'>Goal</p>
-                    <p className='text-xs'>Devops Engineer</p>
+                    <p className='text-very-dark-desaturated-blue font-bold text-lg'>
+                        Expertise Level
+                    </p>
+                    <p className='text-xs'>{expertiseLevel}</p>
                 </div>
             </div>
         </div>
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,24 +1,27 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 
 import UserCard from '../components/UserCard'
 
 const Profile = () => {
+    const [profile, setProfile] = useState('')
+
+    const [name, setName] = useState('')
+    const [age, setAge] = useState('')
+
     const [profession, setProfession] = useState('')
-    const [goalToAchieve, setGoalToAchieve] = useState('')
     const [expertiseLevel, setexpertiseLevel] = useState('')
-    const [dailyTime, setdailyTime] = useState('')
 
     let navigate = useNavigate()
 
-    const createUserProfile = async (profession, goalToAchieve, expertiseLevel, dailyTime) => {
-        const userInfoFromStorage = localStorage.getItem('userInfo')
-            ? JSON.parse(localStorage.getItem('userInfo'))
-            : null
+    const userInfoFromStorage = localStorage.getItem('userInfo')
+        ? JSON.parse(localStorage.getItem('userInfo'))
+        : null
 
-        const token = userInfoFromStorage?.access || ''
+    const token = userInfoFromStorage?.access || ''
 
+    const createUserProfile = async (name, age, profession, expertiseLevel) => {
         try {
             const config = {
                 headers: {
@@ -29,7 +32,7 @@ const Profile = () => {
 
             const { data } = await axios.post(
                 '/api/profile/',
-                { profession, goalToAchieve, expertiseLevel, dailyTime },
+                { name, age, profession, expertiseLevel },
                 config
             )
 
@@ -45,101 +48,131 @@ const Profile = () => {
 
     const submitHandler = async (e) => {
         e.preventDefault()
-        await createUserProfile(profession, goalToAchieve, expertiseLevel, dailyTime)
+        await createUserProfile(name, age, profession, expertiseLevel)
     }
+
+    useEffect(() => {
+        if (!userInfoFromStorage) {
+            navigate('/login')
+        }
+
+        const fetchUserProfile = async () => {
+            const config = {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+            const { data } = await axios.get('/api/profile/', config)
+            setProfile(data)
+        }
+
+        fetchUserProfile()
+    }, [])
 
     return (
         <div className='dark:text-gray-50 max-w-4xl mx-auto'>
-            <UserCard />
-            <span className='block text-xl font-medium mb-2 whitespace-nowrap dark:text-white'>
-                User Profile
-            </span>
-            <form>
-                <div className='grid lg:grid-cols-2 lg:gap-6'>
-                    <div className='relative z-0 w-full mb-6 group'>
-                        <input
-                            type='text'
-                            name='profession'
-                            id='user_profession'
-                            value={profession}
-                            className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
-                            placeholder=' '
-                            required=''
-                            onChange={(e) => setProfession(e.target.value)}
-                        />
-                        <label
-                            htmlFor='user_profession'
-                            className='peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
+            {console.log('profile')}
+            {profile ? (
+                <UserCard
+                    name={profile.name}
+                    username={profile.username}
+                    profession={profile.profession}
+                    expertiseLevel={profile.expertiseLevel}
+                />
+            ) : (
+                <>
+                    <span className='block text-xl font-medium mb-2 whitespace-nowrap dark:text-white'>
+                        Create User Profile
+                    </span>
+                    <form>
+                        <div className='grid lg:grid-cols-2 lg:gap-6'>
+                            <div className='relative z-0 w-full mb-6 group'>
+                                <input
+                                    type='text'
+                                    name='name'
+                                    id='user_name'
+                                    value={name}
+                                    className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
+                                    placeholder=' '
+                                    required=''
+                                    onChange={(e) => setName(e.target.value)}
+                                />
+                                <label
+                                    htmlFor='user_name'
+                                    className='peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
+                                >
+                                    Name
+                                </label>
+                            </div>
+                            <div className='relative z-0 w-full mb-6 group'>
+                                <input
+                                    type='number'
+                                    name='age'
+                                    id='user_age'
+                                    value={age}
+                                    className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
+                                    placeholder=' '
+                                    required=''
+                                    onChange={(e) => setAge(e.target.value)}
+                                />
+                                <label
+                                    htmlFor='user_profession'
+                                    className='peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
+                                >
+                                    Your Age
+                                </label>
+                            </div>
+                        </div>
+                        <div className='grid lg:grid-cols-2 lg:gap-6'>
+                            <div className='relative z-0 w-full mb-6 group'>
+                                <input
+                                    type='text'
+                                    name='profession'
+                                    id='user_profession'
+                                    value={profession}
+                                    className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
+                                    placeholder=' '
+                                    required=''
+                                    onChange={(e) => setProfession(e.target.value)}
+                                />
+                                <label
+                                    htmlFor='user_profession'
+                                    className='peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
+                                >
+                                    Profession (Ex. Teacher)
+                                </label>
+                            </div>
+                            <div className='relative z-0 w-full mb-6 group'>
+                                <input
+                                    type='text'
+                                    // pattern='[0-9]{3}-[0-9]{3}-[0-9]{4}'
+                                    name='expertiseLevel'
+                                    id='user_expertise'
+                                    value={expertiseLevel}
+                                    className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
+                                    placeholder=' '
+                                    required=''
+                                    onChange={(e) => setexpertiseLevel(e.target.value)}
+                                />
+                                <label
+                                    htmlFor='user_expertise'
+                                    className='peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
+                                >
+                                    Your Expertise level (Ex. Beginner)
+                                </label>
+                            </div>
+                        </div>
+                        <button
+                            type='submit'
+                            onClick={submitHandler}
+                            className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
                         >
-                            Profession
-                        </label>
-                    </div>
-                    <div className='relative z-0 w-full mb-6 group'>
-                        <input
-                            type='text'
-                            name='goalToAchieve'
-                            id='user_profession'
-                            value={goalToAchieve}
-                            className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
-                            placeholder=' '
-                            required=''
-                            onChange={(e) => setGoalToAchieve(e.target.value)}
-                        />
-                        <label
-                            htmlFor='user_profession'
-                            className='peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
-                        >
-                            Your Goal (Ex. Web developer)
-                        </label>
-                    </div>
-                </div>
-                <div className='grid lg:grid-cols-2 lg:gap-6'>
-                    <div className='relative z-0 w-full mb-6 group'>
-                        <input
-                            type='text'
-                            // pattern='[0-9]{3}-[0-9]{3}-[0-9]{4}'
-                            name='expertiseLevel'
-                            id='user_expertise'
-                            value={expertiseLevel}
-                            className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
-                            placeholder=' '
-                            required=''
-                            onChange={(e) => setexpertiseLevel(e.target.value)}
-                        />
-                        <label
-                            htmlFor='user_expertise'
-                            className='peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
-                        >
-                            Your Expertise level (Ex. Beginner)
-                        </label>
-                    </div>
-                    <div className='relative z-0 w-full mb-6 group'>
-                        <input
-                            type='text'
-                            name='dailyTime'
-                            id='user_daily_time_hrs'
-                            value={dailyTime}
-                            className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
-                            placeholder=' '
-                            required=''
-                            onChange={(e) => setdailyTime(e.target.value)}
-                        />
-                        <label
-                            htmlFor='user_daily_time_hrs'
-                            className='peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
-                        >
-                            Daily Allocated Time (Ex. 2 hrs)
-                        </label>
-                    </div>
-                </div>
-                <button
-                    type='submit'
-                    onClick={submitHandler}
-                    className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
-                >
-                    Submit
-                </button>
-            </form>
+                            Submit
+                        </button>
+                    </form>
+                </>
+            )}
         </div>
     )
 }
